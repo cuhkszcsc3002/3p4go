05cfd313c532cf6bdb6a64e10447217983b416e1
cuhkszcsc3002 <49808472+cuhkszcsc3002@users.noreply.github.com>
2019-05-18 01:49:28 +0800

    jxr

--- chessboard.cpp
+++ chessboard.cpp
@@ -187,7 +187,7 @@ void chessBoard::DrawItems()
         string strPlay = Stone[i].substr(second+1, static_cast<unsigned int>(Stone.size()));
 
         int x = atoi(strX.c_str());
-   /     int y = atoi(strY.c_str());
+        int y = atoi(strY.c_str());
         int player = atoi(strPlay.c_str());
 
         /* Draw the chess stones if it is within the chessboard */
